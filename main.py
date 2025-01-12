8d92331e7e7e77871c5bbaf221ef316741f8c59e
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-12 11:18:31 +0200

    Crated main.py.

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+# from cheese import router as cheese_router
+# from notes import router as notes_router
+
+app = FastAPI()
+
+# app.include_router(cheese_router.router)
+# app.include_router(notes_router.router)
+
+
+@app.get("/")
+def root() -> dict:
+    return {"message": "Hello Max!"}
