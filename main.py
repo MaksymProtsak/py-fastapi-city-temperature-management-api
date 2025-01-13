147c32905917ee3ec70d20f831c1a1d588a2c9e8
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-13 19:22:03 +0200

    Included router city_router.router.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 
-# from cheese import router as cheese_router
-# from notes import router as notes_router
+from city import router as city_router
 
 app = FastAPI()
 
-# app.include_router(cheese_router.router)
-# app.include_router(notes_router.router)
+app.include_router(city_router.router)
 
 
 @app.get("/")
