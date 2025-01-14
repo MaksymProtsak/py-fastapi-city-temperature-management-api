6c87776517217873491262b2bea1033f1deefdfc
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-14 20:56:42 +0200

    Created create_city cuntion with route cities.

--- city/router.py
+++ city/router.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -11,3 +13,15 @@ router = APIRouter()
 @router.get("/cities/", response_model=list[schemas.City])
 async def read_cities(db: Session = Depends(get_db)):
     return await crud.get_all_cities(db=db)
+
+
+@router.post("/cities/", response_model=schemas.City)
+async def create_city(city: schemas.CityCreate, db: Session = Depends(get_db)):
+    db_city = await crud.get_city_by_name(db=db, city_name=city.name)
+
+    if db_city:
+        raise HTTPException(
+            status_code=400, detail="City already exists"
+        )
+
+    return await crud.create_city(db=db, city=city)
