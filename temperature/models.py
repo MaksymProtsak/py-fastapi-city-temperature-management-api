8a0246925fd392cc391d36a421becef29b8c219a
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-16 16:30:59 +0200

    Added nullable False param for date_time & temperature.

--- temperature/models.py
+++ temperature/models.py
@@ -15,8 +15,8 @@ class DBTemperature(Base):
     __tablename__ = "temperature"
     id = Column(Integer, primary_key=True, index=True)
     city_id = Column(Integer, ForeignKey("city.id"), nullable=False)
-    date_time = Column(DateTime, nullable=False)
-    temperature = Column(Float, nullable=False)
+    date_time = Column(DateTime, nullable=True)
+    temperature = Column(Float, nullable=True)
 
 
 city = relationship("DBCity", back_populates="temperatures")
