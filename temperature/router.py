be331e69b1af7575d171ff4e37c0636f21905c2e
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-17 18:09:45 +0200

    Created update_temperature function with route '/temperatures/update/'.

--- temperature/router.py
+++ temperature/router.py
@@ -1,12 +1,23 @@
+import asyncio
+
+import aiohttp
+
 from fastapi import HTTPException
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends
+)
 
 from sqlalchemy.orm import Session
 
 from dependencies import get_db
 
 from temperature import schemas, crud
-from city.crud import get_city
+from city.crud import (
+    get_city,
+    get_all_cities,
+    update_cities_temperature
+)
 
 router = APIRouter()
 
@@ -28,6 +39,7 @@ async def create_temperature(
         db=db, city_id=temperature.city_id
     )
     city = await get_city(db=db, city_id=temperature.city_id)
+
     if city is None:
         raise HTTPException(
             status_code=400, detail="City not exist"
@@ -42,3 +54,28 @@ async def create_temperature(
         temperature=temperature
     )
     return result
+
+
+@router.post("/temperatures/update/", response_model=list[schemas.Temperature])
+async def update_temperature(db: Session = Depends(get_db)):
+    api_key = "bff8d32b314b46069b7210605251501"
+    cities_temperature = {}
+    cities = await get_all_cities(db=db)
+    urls = [
+        f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city.name}"
+        for city in cities
+    ]
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [session.get(url) for url in urls]
+        results = await asyncio.gather(*tasks)
+
+    for result in results:
+        r_json = await result.json()
+        city_name = r_json["location"]["name"]
+        city_temperature = r_json["current"]["temp_c"]
+        cities_temperature[city_name] = city_temperature
+
+    await update_cities_temperature(db, cities_temperature)
+
+    return await crud.get_all_temperatures(db)
